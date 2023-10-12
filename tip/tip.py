aea2eaab38501634f9a0d4e93b0849deeb14b41f
Eduardo Silva <98997867+Hotchner@users.noreply.github.com>
2023-10-12 21:30:42 +0000

    Thu, Oct 12, 2023, 6:30 PM -03:00

--- tip/tip.py
+++ tip/tip.py
@@ -1,12 +1,15 @@
+#Creating main function
 def main():
     dollars = dollars_to_float(input("How much was the meal? "))
     percent = percent_to_float(input("What percentage would you like to tip?"))
     tip = dollars * percent
     print(f"Leave ${tip:.2f}")
 
+#Function to convert string in float number
 def dollars_to_float(d):
     return float(d.replace("$", ""))
 
+#Function to convert 
 def percent_to_float(p):
     return int(p.replace("%", "")) / 100
 
