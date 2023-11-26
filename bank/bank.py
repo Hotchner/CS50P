a3b62fea189fe12cd18e903b21fa1f84fc31862c
Hotchner <davylf4@gmail.com>
2023-11-25 22:25:48 -0300

    new problem

---
+++ bank/bank.py
@@ -0,0 +1,13 @@
+def hello():
+    word = input("Greeting: ")
+
+    if "Hello" in word:
+        print("$ 0")
+    
+    elif word[0] == "W" or "W":
+        print("$ 100")
+
+    else:
+        print("$ 20")
+
+hello()
