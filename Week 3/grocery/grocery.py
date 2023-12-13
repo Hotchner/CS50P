91060b5c683d5e7b442535f75f8b94ba14bab7f8
Hotchner <davylf4@gmail.com>
2023-12-12 23:12:37 -0300

    update files

---
+++ Week 3/grocery/grocery.py
@@ -0,0 +1,27 @@
+def main():
+    carrinho = {}
+
+    while True:
+        try:
+            produto = input("").lower()
+
+            if not carrinho:
+                carrinho.update({f'{produto}': 1})
+                continue
+            
+            elif produto in carrinho:
+                carrinho[f'{produto}'] +=1
+                continue
+
+            else:
+                carrinho.update({f'{produto}': 1})
+                continue
+        
+        except EOFError:
+            carrinho_ordenado = sorted(carrinho)
+            for _ in carrinho_ordenado:
+                print(f"{carrinho[f'{_}']} {_.upper()}")
+            break
+
+if __name__ =="__main__":
+    main()
---
+++ Week 3/taqueria/taqueria.py
@@ -0,0 +1,38 @@
+menu = {
+    "Baja Taco": 4.25,
+    "Burrito": 7.50,
+    "Bowl": 8.50,
+    "Nachos": 11.00,
+    "Quesadilla": 8.50,
+    "Super Burrito": 8.50,
+    "Super Quesadilla": 9.50,
+    "Taco": 3.00,
+    "Tortilla Salad": 8.00
+}
+
+def main():
+
+    total = 0
+
+    while True:
+        item = input("Item: ").title()
+
+        if item in menu:
+            total = total + menu[f"{item}"]
+            print(f"Total: ${total:.2f}")
+            continue
+
+        elif not item:
+            raise EOFError
+
+        else:
+            print(f"Total: ${total:.2f}")
+            continue
+
+
+while True:
+    try:
+        main()
+    except (EOFError):
+        break
+    
