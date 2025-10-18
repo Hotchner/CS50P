d5a1da8f8c7442303341446d820c4d4f130ebd8c
Hotchner <davylf4@gmail.com>
2025-10-18 16:35:35 -0300

    complete

--- shirt/main.py
+++ shirt/main.py
@@ -4,19 +4,26 @@ import os
 
 
 def main():
-     # #Menos de 1 argumento
-     # if len(sys.argv) < 3:
-     #      sys.exit("Too few command-line arguments")
-     # #Mais de 2 argumento
-     # elif len(sys.argv) > 3:
-     #      sys.exit("Too many command-line arguments")
-     
-     formatos_aceitos = [".jpg", ".jpeg", ".png"]
-          
-     # input_name_1, output_type_1 = os.path.splitext(sys.argv[1])
-     # file_name_2, file_type_2 = os.path.splitext(sys.argv[2])
+     #Menos de 1 argumento
+     if len(sys.argv) < 3:
+          sys.exit("Too few command-line arguments")
+     #Mais de 2 argumento
+     elif len(sys.argv) > 3:
+          sys.exit("Too many command-line arguments")
 
-     #photo_1 = Image.open(sys.argv[1]).convert("RGBA")
+     formatos_aceitos = [".jpg", ".jpeg", ".png"]
+
+     input_name, input_type = os.path.splitext(sys.argv[1])
+     output_name, output_type = os.path.splitext(sys.argv[2])
+
+     if output_type not in formatos_aceitos:
+          sys.exit("Invalid output")
+     elif input_type != output_type:
+          sys.exit("Input and output have different extensions")
+     elif os.path.exists(sys.argv[1]) == False:
+          sys.exit("Input does not exist") 
+     else:
+          pass
 
      photo = Image.open(sys.argv[1])
      cs50_shirt = Image.open("shirt.png").convert("RGBA") #PNG da camisa
@@ -24,10 +31,7 @@ def main():
      photo2 = ImageOps.fit(photo, cs50_shirt.size)
      photo2.paste(cs50_shirt, (0, 0), cs50_shirt)
 
+     photo2.save(f"{output_name}{output_type}")
      photo2.show()
-     # if file_type_1 != file_type_2:
-     #      sys.exit("Input and output have different extensions")
-     # elif 
-
 
 main()
